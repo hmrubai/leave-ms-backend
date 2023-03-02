7b56fede73721dca9600e296f91c82b9892aa1d4
hmrubai <rubai.mobarak@gmail.com>
2023-03-02 10:04:41 +0600

    Approval ID added

--- app/Http/Controllers/LeaveApplicationController.php
+++ app/Http/Controllers/LeaveApplicationController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use App\Models\User;
+use App\Models\LeaveApplications;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class LeaveApplicationController extends Controller
 {
